8e86a5e0f09acb5b191f67a90148add344734a12
Anurag Singh <anuragsinghrkt1005@gmail.com>
2021-03-21 18:01:25 +0530

    refined

---
+++ Algo/Array_Search/Interpolation.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define PI 3.14159265
+#define INF 1e16 + 5
+#define int long long
+using namespace std;
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
