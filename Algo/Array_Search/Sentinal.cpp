6bc6528a7dcead87a2f5344811a44cd1c21a8c4c
Anurag Singh <anuragsinghrkt1005@gmail.com>
2021-03-21 18:49:24 +0530

    without fast i/o

--- Algo/Array_Search/Sentinal.cpp
+++ Algo/Array_Search/Sentinal.cpp
@@ -1,6 +1,25 @@
 #include <bits/stdc++.h>
+#define PI 3.14159265
+#define INF 1e16 + 5
+#define int long long
 using namespace std;
-int main()
+int32_t main()
 {
-    return 0;
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+
+    int N;
+    vector<int> arr;
+    printf("Enter No of element into the array \n");
+    //cout << "Enter No of element into the array" << '\n';
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        int temp;
+        cin >> temp;
+        arr.push_back(temp);
+    }
+    cout << arr[0] << '\n';
+
+    return EXIT_SUCCESS;
 }
